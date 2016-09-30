47316dd2886168c9d21cee60836d03510512ae69
Ryan Barry <ryan.barry.ms@outlook.com>
2016-09-29 19:57:13 -0500

    Added another amount of text

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ module.exports = function(context, req) {
     else {
         context.res = {
             status: 400,
-            body: "Please yo pass a name on the query string or in the request body"
+            body: "Please yo yo pass a name on the query string or in the request body"
         };
     }
     context.done();
